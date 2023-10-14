32235ace3800473ba609c770da7eb7d6e798b35e
abishekkiruba <147931772+abishekkiruba@users.noreply.github.com>
2023-10-14 20:19:39 +0530

    a2

---
+++ Abishek portfolio/src.js
@@ -0,0 +1,18 @@
+const insta = () => {
+
+    window.open("https://www.instagram.com/hardik_abishek33/" , '_blank');
+};
+
+const linkedin = () => {
+
+    window.open("https://www.linkedin.com/in/abishek-kirubanandhan-674669296?trk=contact-info",'_blank');
+};
+
+const twitter = () => {
+
+    window.open("https://twitter.com/abishek_1993" , '_blank');
+};
+
+const resume = (resume) => {
+    window.open(resume, "_blank");
+}
